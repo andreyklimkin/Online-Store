5febe753b43204e69a558107dd13f53d0dd6e5e9
andreyklimkin <andreyklimkin@yandex.ru>
2016-02-20 18:36:23 +0300

    Src

---
+++ SRC/blog/models.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Article(models.Model):
+    class Meta():
+        db_table = "article"
+    
+    article_title = models.CharField(max_length = 200)
+    article_text = models.TextField()
+    article_date = models.DateTimeField()
+    article_likes = models.IntegerField(default=0)
+
+
+class Comments(models.Model):
+    class Meta():
+        db_table = "comments"
+
+    comments_text = models.TextField()
+    comments_article = models.ForeignKey(Article)
+
+
